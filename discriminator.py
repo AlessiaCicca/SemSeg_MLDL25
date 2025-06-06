6b5bb7912d6584cfdedf397d5feeaea49fb49e06
Alessia Ciccaglione <162203648+AlessiaCicca@users.noreply.github.com>
2025-06-06 06:36:03 +0200

    Update discriminator.py

--- discriminator.py
+++ discriminator.py
@@ -1,3 +1,4 @@
+'''
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -32,3 +33,35 @@ class FCDiscriminator(nn.Module):
   #x = self.sigmoid(x) 
 
   return x
+'''
+import torch.nn as nn
+
+import torch.nn as nn
+
+class FCDiscriminator(nn.Module):
+    def __init__(self, num_classes, ndf=32):  # NDF ridotto
+        super(FCDiscriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(num_classes, ndf, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout2d(0.2),
+
+            nn.Conv2d(ndf, ndf*2, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout2d(0.2),
+
+            nn.Conv2d(ndf*2, ndf*4, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout2d(0.2),
+
+            nn.Conv2d(ndf*4, ndf*8, kernel_size=4, stride=1, padding=1),  # più risoluzione
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(ndf*8, 1, kernel_size=4, stride=1, padding=1),
+            # nn.Sigmoid()  # se NON usi BCEWithLogitsLoss
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
