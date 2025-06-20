65a44c309a2917794f16ce88305e163f8550a78d
Alessia Ciccaglione <162203648+AlessiaCicca@users.noreply.github.com>
2025-06-20 14:00:12 +0200

    Update trainDeeplab.py

--- trainDeeplab.py
+++ trainDeeplab.py
@@ -14,6 +14,29 @@ import datasets.cityscapes as cityscapes
 
 scaler = GradScaler()  
 
+def compute_miou(preds, labels, num_classes=19, ignore_index=255):
+    ious = []
+    preds = preds.cpu().numpy()
+    labels = labels.cpu().numpy()
+
+    for cls in range(num_classes):
+        if cls == ignore_index:
+            continue
+        pred_inds = (preds == cls)
+        label_inds = (labels == cls)
+
+        intersection = np.logical_and(pred_inds, label_inds).sum()
+        union = np.logical_or(pred_inds, label_inds).sum()
+
+        if union == 0:
+            continue
+        iou = intersection / union
+        ious.append(iou)
+
+    if len(ious) == 0:
+        return 0.0
+    return np.mean(ious)
+
 def train(epoch, model, train_loader, criterion, optimizer, device):
     model.train()
     running_loss, correct, total = 0.0, 0, 0
@@ -44,9 +67,11 @@ def train(epoch, model, train_loader, criterion, optimizer, device):
     print(f'Train Epoch {epoch} - Loss: {running_loss / len(train_loader):.4f} - Acc: {acc:.2f}%')
 
 
-def validate(model, val_loader, criterion, device):
+def validate(model, val_loader, criterion, device, num_classes=19):
     model.eval()
     val_loss, correct, total = 0, 0, 0
+    miou_total = 0
+    count = 0
 
     with torch.no_grad():
         for inputs, targets in val_loader:
@@ -60,12 +85,17 @@ def validate(model, val_loader, criterion, device):
             correct += (predicted == targets.squeeze(1)).sum().item()
             total += torch.numel(targets.squeeze(1))
 
+            miou_batch = compute_miou(predicted, targets.squeeze(1), num_classes=num_classes)
+            miou_total += miou_batch
+            count += 1
+
             del inputs, targets, outputs, predicted, loss
             gc.collect()
 
     acc = 100. * correct / total
-    print(f'Validation - Loss: {val_loss / len(val_loader):.4f} - Acc: {acc:.2f}%')
-    return acc
+    mean_iou = 100. * (miou_total / count) if count > 0 else 0
+    print(f'Validation - Loss: {val_loss / len(val_loader):.4f} - Acc: {acc:.2f}% - mIoU: {mean_iou:.2f}%')
+    return acc, mean_iou
 
 
 def find_folder(start_path, folder_name):
@@ -76,19 +106,10 @@ def find_folder(start_path, folder_name):
 
 
 if __name__ == "__main__":
-    print(">>> Avvio training...")
+    print(">>> Training...")
 
     zip_path = 'cityscapes_dataset.zip'
     base_extract_path = './Cityscapes'
-
-    """if not os.path.exists(base_extract_path):
-        print(" Dataset non trovato o incompleto, lo scarico...")
-        os.system(f"gdown https://drive.google.com/uc?id=1Qb4UrNsjvlU-wEsR9d7rckB0YS_LXgb2 -O {zip_path}")
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            zip_ref.extractall(base_extract_path)
-        print(" Estrazione completata.")
-    else:
-        print(" Dataset già presente.")"""
 
     images_dir = find_folder(base_extract_path, 'images')
     masks_dir = find_folder(base_extract_path, 'gtFine')
@@ -128,11 +149,12 @@ if __name__ == "__main__":
     num_epochs = 50
     learning_rates = [0.000025, 0.001]
     batch_sizes = [1, 4, 8]
+    #Best configuration lr=0.001 and bs=4
  
 
     for lr in learning_rates:
         for bs in batch_sizes:
-            print(f"\n>>> Inizio training con lr={lr}, batch_size={bs}")
+            print(f"\n>>> Training with lr={lr} and batch_size={bs}")
 
             train_loader = DataLoader(train_dataset, batch_size=bs, shuffle=True,
                                       num_workers=2, pin_memory=True)  
@@ -143,9 +165,11 @@ if __name__ == "__main__":
             criterion = nn.CrossEntropyLoss(ignore_index=255)
             optimizer = optim.SGD(model.optim_parameters(lr = lr), lr=lr, momentum=0.9, weight_decay=5e-4)
 
-            best_acc = 0
+            
             best_lr=0
             best_bs=0
+            best_acc = 0
+            best_miou = 0
 
             for epoch in range(num_epochs):
                 print(f"\nEpoch {epoch+1}/{num_epochs}")
@@ -154,14 +178,15 @@ if __name__ == "__main__":
 
                 if epoch % 10 == 0 or epoch == num_epochs - 1:
                     torch.save(model.state_dict(), f'checkpoints/checkpoint_epoch_{epoch}_lr{lr}_bs{bs}.pth')
-                    print(f"Salvato checkpoint epoch {epoch}")
+                    print(f"checkpoint epoch {epoch}")
 
-                if val_acc > best_acc:
-                    best_acc = val_acc
+                if val_miou > best_miou:
+                    best_miou = val_miou
                     best_lr=lr
                     best_bs=bs
-                    torch.save(model.state_dict(), f'checkpoints/best_model_lr{lr}_bs{bs}.pth')
-                    print(f"Nuovo best model con acc: {best_acc:.2f}%")
+                    torch.save(model.state_dict(), f'checkpoints/best_model_lr{lr}_bs{bs}.pth
+                    print(f"Best model -> mIoU: {best_miou:.2f}% (Acc: {val_acc:.2f}%)")
+                    print(f"Nuovo best model con acc: {val_acc:.2f}%")
 
 
-            print(f" Fine training per lr={best_lr}, bs={best_bs} | Best Acc: {best_acc:.2f}%")
+            print(f" Fine training per lr={best_lr}, bs={best_bs} | Best mIou: {best_miou:.2f}%")
