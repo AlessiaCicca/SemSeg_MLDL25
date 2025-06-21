a5317ba2b9e06fb8256372f63892b0e91f1a4dfd
Alessia Ciccaglione <162203648+AlessiaCicca@users.noreply.github.com>
2025-06-21 08:09:42 +0200

    Update evaluationBiseNet.py

--- evaluationBiseNet.py
+++ evaluationBiseNet.py
@@ -127,7 +127,7 @@ if __name__ == "__main__":
     )
     val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False, num_workers=2)
     model = BiSeNet(num_classes=19, context_path='resnet18').to(device)
-    model.load_state_dict(torch.load('/content/SemSeg_MLDL25/checkpoints/final_model_lr2.5e-05_bs4.pth', map_location=device))
+    model.load_state_dict(torch.load('/content/SemSeg_MLDL25/best_model.pth', map_location=device))
     model.eval()
 
 
